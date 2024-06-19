e6105962077665e5be1c856dce4697ffa625f31b
Vikas <vikask4590@gmail.com>
2024-06-20 03:15:51 +0530

    fixed

--- src/components/landing-page/index.jsx
+++ src/components/landing-page/index.jsx
@@ -4,10 +4,12 @@ import About from "./about";
 import HeroSection from "./hero-section";
 import Tables from "./tables";
 import Faqs from "./faqs";
+import styles from "./style.module.scss";
 
 function LandingPage() {
   return (
     <div className="container">
+      <div className={styles.left}></div>
       <HeroSection />
       <About />
       <Tables />
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./navbar.module.scss";
 import HamBurger from "@/svg/hamburger.svg";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const nav = [
   {
@@ -28,10 +28,41 @@ const nav = [
   },
 ];
 
-const time = "2024-12-31T00:00:00";
+const time = "2024-07-30T00:00:00";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const calculateTimeLeft = () => {
+    const difference = +new Date(time) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    setDays(timeLeft.days);
+    setHours(timeLeft.hours);
+    setMinutes(timeLeft.minutes);
+    setSeconds(timeLeft.seconds);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      calculateTimeLeft();
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <nav className={styles.navbar + " container"}>
@@ -55,6 +86,18 @@ function Navbar() {
               </li>
             ))}
           </ul>
+          <div className={styles.time}>
+            <h3>pre-sale start</h3>
+            <div className={styles.timer}>
+              <div>{days}</div>
+              <span>:</span>
+              <div>{hours}</div>
+              <span>:</span>
+              <div>{minutes}</div>
+              <span>:</span>
+              <div>{seconds}</div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
